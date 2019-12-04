52915284e58132356953b6f1b6a2f59dc6060e64
Delvric Tezeno <delvric@delvrics-mbp.home>
2019-12-04 11:33:13 -0600

    card

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import './App.css';
+import {Button, Card, Icon, Image} from 'semantic-ui-react'
+
+
+const github_user="http://api.github.com/users/Delvric"
+
+
+
+class App extends Component {
+  state = {
+    user: {},
+    active: false
+  }
+
+  card = () => {
+  console.log('Button clicked')
+
+  if(this.state.active === true){
+    this.setState({active: false})
+  }
+
+  else {
+    fetch(github_user)
+    .then(response => response.json())
+    .then(user => {
+      this.setState({user, active: true})
+    })
+  }
+}
+  render() {
+    return (
+<React.Fragment>
+  {console.log(this.state.user)}
+  <Button onClick = {this.card}> click me</Button>
+  {this.state.active === true &&(
+    <Card>
+    <Image src = {this.state.user.avatar_url} wrapped ui = {false} />
+    <Card.Content>
+      <Card.Header>{this.state.user.name}</Card.Header>
+      <Card.Meta>
+        <span className = "date">Joined in 08/2019</span>
+      </Card.Meta>
+      <Card.Description>
+        {this.state.user.bio}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <Icon name = "user" />
+        Followers: {this.state.user.followers}
+    </Card.Content>
+  </Card>
+  )}
+</React.Fragment>
+  );
+}
+}
+
+    
+
+export default App;
